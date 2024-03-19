bdaafb0a76f4e7b5eb1e6d7d3b0c76cf61e9e23b
angelafort <139291628+angelafort@users.noreply.github.com>
2024-03-19 02:15:41 +0000

    First commit

--- src/app.js
+++ src/app.js
@@ -6,6 +6,22 @@ import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
 window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
+  let pronouns = ["the", "our", "my", "a"];
+  let adjectives = ["great", "big", "amazing", "cool"];
+  let nouns = ["jogger", "raccoon", "cat", "pencil"];
+  let extensions = [".com", ".net", ".us", ".ve"];
+
+  function generateRandomDomain() {
+    let randomPronoun = pronouns[Math.floor(Math.random() * pronouns.length)];
+    let randomAdjective =
+      adjectives[Math.floor(Math.random() * adjectives.length)];
+    let randomNoun = nouns[Math.floor(Math.random() * nouns.length)];
+    let randomExtension =
+      extensions[Math.floor(Math.random() * extensions.length)];
+
+    return `${randomPronoun}${randomAdjective}${randomNoun}${randomExtension}`;
+  }
+
+  let domainElement = document.getElementById("domainName");
+  domainElement.textContent = generateRandomDomain();
 };
